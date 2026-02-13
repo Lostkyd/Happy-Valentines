c105a72a3c78881a4040e57a72553334b365dfa3
Lostkyd <ronnkevinrubio@gmail.com>
2026-02-13 23:19:43 +0800

    Added Confetti Effect and Modify Design for cards

--- valentine-entry/src/components/email/EmailForm.jsx
+++ valentine-entry/src/components/email/EmailForm.jsx
@@ -18,10 +18,8 @@ export default function EmailForm({ onSent }) {
     formData.append('access_key', '7df23f42-30ec-4557-927e-f4049f767485');
     formData.append('subject', 'Happy Valentine\'s Day!');
     formData.append('from_name', 'Ronn Kevin A. Rubio');
-    formData.append('to_email', email);
     const message = `Happy Valentine's Day, my love! 💕\n\nDate Information:\n📅 Date: 02/16/2026 (Monday)\n⏰ Time: 1:00 PM - 5:00 PM\n📍 Venue: Farmer's Cubao - Wok It Out\n\n✨ Wear your best OOTD — I want to see you shine!\n💞 I love you so much.\n\n🌹 Can't wait to see you! 🌹`;
     formData.append('message', message);
-    formData.append('reply_to', email);
 
     try {
       const res = await fetch('https://api.web3forms.com/submit', {
